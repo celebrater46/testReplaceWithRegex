e71821b7c8b82952a8b650cbae6fd20f5b35a579
celebrater <tkgm45nbchk@gmail.com>
2022-02-17 03:41:53 +0900

    decoded ruby correctly

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const str = "<6>大晦日</6>";
 const str2 = "<3>大晦日</3>です、<5>ヒデル</5>様！！";
 const str3 = "人間の息子・｜堕天男《ルシファー》と、天使の母・｜沙麻《サーマ》。";
+const str4 = "人間の息子・<ruby><rb>堕天男</rb><rp>(</rp><rt>ルシファー</rt><rp>)</rp></ruby>と、天使の母・<ruby><rb>沙麻</rb><rp>(</rp><rt>サーマ</rt><rp>)</rp></ruby>。";
 //$ptn = "/\<[1-7]\>(.*)\<\/[1-7]\>/u";
 // const ptn = "/\<([1-7])\>(.*)\<\/.\>/g";
 // const rp = "<span class='f$1'>$2</span>";
@@ -35,6 +36,12 @@ const replaced3 = str3.replace(
     "<ruby><rb>$1</rb><rp>(</rp><rt>$2</rt><rp>)</rp></ruby>"
 );
 
+const replaced4 = str4.replace(
+    /<ruby><rb>([^\x01-\x7E]+)<\/rb><rp>\(<\/rp><rt>([^\x01-\x7E]+)<\/rt><rp>\)<\/rp><\/ruby>/g,
+    "｜$1《$2》"
+);
+
 console.log(replaced);
 console.log(replaced2);
 console.log(replaced3);
+console.log(replaced4);
