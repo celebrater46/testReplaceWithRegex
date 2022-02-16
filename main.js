a1215dfa07798d9c59ebe73218935d5b24588a2c
celebrater <tkgm45nbchk@gmail.com>
2022-02-16 19:56:00 +0900

    test succeeded again

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
 const str = "<6>大晦日</6>";
 //$ptn = "/\<[1-7]\>(.*)\<\/[1-7]\>/u";
-// const ptn = "/\<([1-7])\>(.*)\<\/.\>/u";
+// const ptn = "/\<([1-7])\>(.*)\<\/.\>/g";
 const rp = "<span class='f$1'>$2</span>";
 
+// const replaced = str.replace(ptn, rp); // <6>大晦日</6>
 const replaced = str.replace(/\<([1-7])\>(.*)\<\/.\>/g, rp); // <span class='f6'>大晦日</span>
 
 console.log(replaced);
